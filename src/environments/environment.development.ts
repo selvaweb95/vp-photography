ec0aa1fcbd7a4bb6b9f74445d6acba74b7d0a546
Sabarinathan004 <154677710+Sabarinathan004@users.noreply.github.com>
2024-03-28 20:27:14 +0530

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    baseUrl : "https://5836-2409-40f4-1029-b6ec-fd2a-5b6c-d4b3-ed3a.ngrok-free.app"
+    baseUrl : "https://www.vasanthamphotography.website"
 };
